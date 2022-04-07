5e9cac145348651287c5fd34fc1c730a51324de7
Patrick Pires <patrick.piresp@gmail.com>
2022-04-06 22:17:40 -0300

    refact: extracting some methods, variables and constants

--- src/main/java/br/com/ptk/App.java
+++ src/main/java/br/com/ptk/App.java
@@ -7,22 +7,44 @@ import java.util.Properties;
 
 public class App {
     private static final String BASE_URL_STRING = "https://imdb-api.com/";
+    private static final String USER_AGENT = "Java-http-client/" + System.getProperty("java.version");
 
     public static void main(String[] args) throws IOException {
         String pathToTop250Movies = "API/Top250Movies/" + getApiKey();
         String urlString = BASE_URL_STRING + pathToTop250Movies;
 
+        HttpURLConnection connection = getConnection(urlString);
+        if (unsuccessfulRequest(connection)) handleUnsuccessfulRequest();
+        printResponseBody(connection);
+
+        connection.disconnect();
+    }
+
+    private static HttpURLConnection getConnection(String urlString) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        connection.setRequestProperty("user-agent", "Java-http-client/" + System.getProperty("java.version"));
+        connection.setRequestProperty("user-agent", USER_AGENT);
+        return connection;
+    }
 
+    private static void printResponseBody(HttpURLConnection connection) throws IOException {
         Reader streamReader = new InputStreamReader(connection.getInputStream());
         BufferedReader br = new BufferedReader(streamReader);
         String line;
         while ((line = br.readLine()) != null) {
             System.out.println(line);
         }
+    }
+
+    private static void handleUnsuccessfulRequest() {
+        // TODO: implement this method change it's name
+        //  throw some exception -> search carefully which exception to throw
+    }
+
+    private static boolean unsuccessfulRequest(HttpURLConnection connection) throws IOException {
+        int statusCode = connection.getResponseCode();
+        return 200 <= statusCode && statusCode < 300;
     }
 
     private static String getApiKey() throws IOException {
